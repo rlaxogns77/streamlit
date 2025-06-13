cd356bcb1289da1fbb0e99657b5c7139f2b2a1c0
rlaxogns77 <hun010510@gmail.com>
2025-06-13 09:24:00 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 import re
-import os
 from openai import AsyncOpenAI, OpenAI
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]  # 🔑 환경변수에서 불러오기
+OPENAI_API_KEY = ""
 
-client = AsyncOpenAI(api_key=OPENAI_API_KEY)
-sync_client = OpenAI(api_key=OPENAI_API_KEY)
+client = AsyncOpenAI(
+    api_key="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"
+)
+
+sync_client = OpenAI(
+    api_key="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"
+)
 
 
 def llm_call(prompt: str,  model: str = "gpt-4o-mini") -> str:
